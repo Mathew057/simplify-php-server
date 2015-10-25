7f3753432f6c09e5c041afcba3ca258fb146ebbd
Mathew057 <mathew0057@gmail.com>
2015-10-25 03:16:46 -0700

    ugh

--- charge.php
+++ charge.php
@@ -50,7 +50,7 @@ $currency = isset($_POST["currency"]) ? $_POST["currency"] : 'USD';
 
 if (isset($customerId)) {
 	$paymentPayload = array(
-		'amount' => "500"//$payment,
+		'amount' => "46"//$payment,
 		'customer' => $token,
 		'description' => 'Test payment',
 		'currency' => $currency
